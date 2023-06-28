93b7364ab525031a61c5dbdf8279a0f4f8debba0
RAHUL SAHU <rahulsa667@gmail.com>
2023-06-28 20:20:04 +0530

    db updated

--- Db/Db.js
+++ Db/Db.js
@@ -5,25 +5,25 @@ import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// dotenv.config({path:__dirname+'.env'});
+dotenv.config({path:__dirname+'.env'});
  
-// export const conn1 = mysql.createConnection({
-//     host: 'localhost',
-//     port: process.env.PORT,
-//     user: 'root',
-//     password:'LexPubSum@1917',
-//     database:'pravri',
-//     connectionLimit:10
-// })
-
-dotenv.config();
 export const conn1 = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.DB_USER,
-    password:process.env.PASSWORD,
-    database:process.env.DATABASE,
-    connectionLimit:10,
-    multipleStatements:true
+    host: 'localhost',
+    port: process.env.PORT,
+    user: 'root',
+    password:'LexPubSum@1917',
+    database:'pravri',
+    connectionLimit:10
 })
+
+// dotenv.config();
+// export const conn1 = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.DB_USER,
+//     password:process.env.PASSWORD,
+//     database:process.env.DATABASE,
+//     connectionLimit:10,
+//     multipleStatements:true
+// })
 
 
